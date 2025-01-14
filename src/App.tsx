1831bd0fa3d8c1f9f73a6693a007d674039894cf
raphaelaugustb <travassosraphael12@gmail.com>
2025-01-13 23:03:21 -0300

    add new modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import "./global.css"
-
+import styles from "./App.module.css"
+import { Header } from "./components/Header/Header"
+import { CreateArea } from "./components/CreateArea/CreateArea"
 function App() {
-  return <div></div>
+  return (
+    <div>
+      <Header></Header>
+      <CreateArea></CreateArea>
+      <div className={styles.taskContainer}>
+        <header>
+          <strong>
+            Tarefas <span>0</span>
+          </strong>
+
+          <strong>
+            Conclúidas <span>0</span>
+          </strong>
+        </header>
+        <div></div>
+      </div>
+    </div>
+  )
 }
 
 export default App
