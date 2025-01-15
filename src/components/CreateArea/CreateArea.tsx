1e84f613e4f24533a2e4962d77ae4333c2f2bfa1
raphaelaugustb <travassosraphael12@gmail.com>
2025-01-15 12:31:28 -0300

    refactor: adding form on text area and refactoring css

--- src/components/CreateArea/CreateArea.tsx
+++ src/components/CreateArea/CreateArea.tsx
@@ -1,13 +1,21 @@
 import styles from "./CreateArea.module.css"
 import { PlusCircle } from "@phosphor-icons/react"
+import { FormEvent } from "react"
+function handleCreateNewTask(event: FormEvent) {}
 export function CreateArea() {
   return (
     <div className={styles.createAreaContainer}>
-      <textarea name="" id="" placeholder="Adicione uma nova tarefa"></textarea>
-      <button>
-        Criar
-        <PlusCircle size={18} />
-      </button>
+      <form onSubmit={handleCreateNewTask} action="">
+        <textarea
+          name=""
+          id=""
+          placeholder="Adicione uma nova tarefa"
+        ></textarea>
+        <button type="submit">
+          Criar
+          <PlusCircle size={18} />
+        </button>
+      </form>
     </div>
   )
 }
