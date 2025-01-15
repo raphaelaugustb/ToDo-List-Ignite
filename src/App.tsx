3facbe44234acb5259765deb1e272868cc2e6b25
raphaelaugustb <travassosraphael12@gmail.com>
2025-01-15 12:31:58 -0300

    feat: create task and css module

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./global.css"
 import styles from "./App.module.css"
 import { Header } from "./components/Header/Header"
 import { CreateArea } from "./components/CreateArea/CreateArea"
+import { Task } from "./components/Task/Task"
 function App() {
   return (
     <div>
@@ -17,7 +18,11 @@ function App() {
             Conclúidas <span>0</span>
           </strong>
         </header>
-        <div></div>
+        <div>
+          <Task taskDescription="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
+          <Task taskDescription="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
+          <Task taskDescription="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
+        </div>
       </div>
     </div>
   )
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,2 +1,20 @@
 import styles from "./Task.module.css"
-export function Task() {}
+import { Trash, Check } from "@phosphor-icons/react"
+import { FocusEvent } from "react"
+interface taskProps {
+  taskDescription: string
+}
+
+export function Task({ taskDescription }: taskProps) {
+  return (
+    <div className={styles.taskContainer}>
+      <div className={styles.descriptionContainer}>
+        <button></button>
+        <p>{taskDescription}</p>
+      </div>
+      <button>
+        <Trash size={20} />
+      </button>
+    </div>
+  )
+}
